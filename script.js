9d05eb6bae17d5f211949d6487d3bce9ca2fea02
hartjoa <42883820+hartjoa@users.noreply.github.com>
2024-07-20 15:19:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const diceContainer = document.getElementById('dice-container');
 const rollButton = document.getElementById('roll-button');
 
 const diceSides = [
-  '1-blue.png', '1-white.png', '1-red.png', '1-black.png', '1-green.png', '1-orange.png',
-  '2-blue.png', '2-white.png', '2-red.png', '2-black.png', '2-green.png', '2-orange.png',
-  '3-blue.png', '3-white.png', '3-red.png', '3-black.png', '3-green.png', '3-orange.png',
-  '4-blue.png', '4-white.png', '4-red.png', '4-black.png', '4-green.png', '4-orange.png',
-  '5-blue.png', '5-white.png', '5-red.png', '5-black.png', '5-green.png', '5-orange.png',
-  '6-blue.png', '6-white.png', '6-red.png', '6-black.png', '6-green.png', '6-orange.png'
+  'dice-1-blue', 'dice-1-white', 'dice-1-red', 'dice-1-black', 'dice-1-green', 'dice-1-orange',
+  'dice-2-blue', 'dice-2-white', 'dice-2-red', 'dice-2-black', 'dice-2-green', 'dice-2-orange',
+  'dice-3-blue', 'dice-3-white', 'dice-3-red', 'dice-3-black', 'dice-3-green', 'dice-3-orange',
+  'dice-4-blue', 'dice-4-white', 'dice-4-red', 'dice-4-black', 'dice-4-green', 'dice-4-orange',
+  'dice-5-blue', 'dice-5-white', 'dice-5-red', 'dice-5-black', 'dice-5-green', 'dice-5-orange',
+  'dice-6-blue', 'dice-6-white', 'dice-6-red', 'dice-6-black', 'dice-6-green', 'dice-6-orange'
 ];
 
 rollButton.addEventListener('click', () => {
